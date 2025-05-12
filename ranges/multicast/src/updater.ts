9ad8f47b9c177f8a0ac1c8ec36d6e6b602db1739
Sven Finn <77897281+SvenFinn@users.noreply.github.com>
2025-05-12 14:07:51 +0200

    feat: Make cache interval configurable

--- ranges/multicast/src/updater.ts
+++ ranges/multicast/src/updater.ts
@@ -4,8 +4,16 @@ import { updateShooters } from "./shooter";
 import { LocalClient } from "dc-db-local";
 import { logger } from "dc-logger";
 import { updateOverrides } from "./discipline/overrides";
+import dotenv from "dotenv";
+dotenv.config();
 
 const client = new LocalClient();
+
+if (!process.env.CACHE_REFRESH_INTERVAL) {
+    logger.error("CACHE_REFRESH_INTERVAL is not set");
+    process.exit(1);
+}
+const refreshInterval = parseInt(process.env.CACHE_REFRESH_INTERVAL);
 
 async function update() {
     logger.info("Updating caches");
@@ -15,7 +23,7 @@ async function update() {
         updateStartList(client),
         updateShooters(client)
     ]);
-    setTimeout(update, 60000);
+    setTimeout(update, refreshInterval);
 }
 
 update();
