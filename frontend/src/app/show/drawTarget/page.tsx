c7946770f097ee75c9d675de7eb48a2703aa31ad
Sven Finn <77897281+SvenFinn@users.noreply.github.com>
2025-06-22 16:25:56 +0200

    feat: DrawTarget custom is working

--- frontend/src/app/show/drawTarget/page.tsx
+++ frontend/src/app/show/drawTarget/page.tsx
@@ -1,48 +1,26 @@
 "use client";
 
 import { DrawTargetOptions, isDrawTargetOptions } from "@shared/screens/drawTarget";
-import DrawTargetEdit from "./components/DrawTargetEdit";
-import { useSearchParams } from "next/navigation";
 import DrawTarget from "./components/DrawTarget/drawTarget";
-import { Suspense } from "react";
-
-function SuspenseWrapper() {
-    function handleSubmit(options: DrawTargetOptions) {
-        // Serialize the options to JSON
-        const jsonOptions = JSON.stringify(options);
-
-        // Append the JSON to the URL as a query parameter
-        const url = new URL(window.location.href);
-        url.searchParams.set("options", jsonOptions);
-        window.location.href = url.toString();
-    }
-
-    const searchParams = useSearchParams();
-    const optionsParam = searchParams.get("options");
-    const options = optionsParam ? JSON.parse(optionsParam) : null;
-    const validatedOptions = options && isDrawTargetOptions(options) ? options : null;
-
-
-    return (
-        <>
-            {!validatedOptions ? (
-                <>
-                    <h1>Custom View: DrawTarget</h1>
-                    <p>Create a custom DrawTarget view</p>
-                    <DrawTargetEdit onSubmit={handleSubmit} />
-                </>
-            ) : (
-                <DrawTarget options={validatedOptions} />
-            )}
-        </>
-    );
-}
-
+import SingleEdit from "../components/SingleEdit";
+import { schema, getUiSchema } from "./components/DrawTargetEdit";
 
 export default function Page() {
     return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <SuspenseWrapper />
-        </Suspense>
+        <SingleEdit
+            initialData={
+                {
+                    rows: 1,
+                    columns: 1,
+                    ranges: [],
+                    highlightAssign: false
+                } as DrawTargetOptions
+            }
+            name="Draw Target"
+            schema={schema}
+            uiSchemaFn={getUiSchema}
+            typeCheck={isDrawTargetOptions}
+            renderComponent={(data: DrawTargetOptions) => <DrawTarget options={data} />}
+        />
     );
 }
