e8d78139484e68d6753efceb488e5c26632569b2
Sven Finn <77897281+SvenFinn@users.noreply.github.com>
2025-06-23 21:43:29 +0200

    fix: Prevent buttons from submitting

--- frontend/src/app/components/FileManagers/FileManager/Actions/Buttons/button.tsx
+++ frontend/src/app/components/FileManagers/FileManager/Actions/Buttons/button.tsx
@@ -6,8 +6,14 @@ type ButtonProps = {
 };
 
 export default function Button({ children, onClick }: ButtonProps) {
+    function handleClick(event: React.MouseEvent<HTMLButtonElement>) {
+        event.stopPropagation(); // Prevents the click from bubbling up to parent elements
+        event.preventDefault(); // Prevents the default button behavior
+        onClick();
+    }
+
     return (
-        <button className={styles.button} onClick={onClick}>
+        <button className={styles.button} onClick={handleClick}>
             {children}
         </button>
     );
