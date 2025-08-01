f58642a69e6337d285a5d2cabf581b12cc99e9c9
Sven Finn <77897281+SvenFinn@users.noreply.github.com>
2025-08-01 17:28:17 +0200

    Revert "fix: Remove import"
    This reverts commit af4f46b4f129a6b282750a7bc23a1ff062f5ff9e.

--- frontend/src/app/show/components/Fullscreen/index.tsx
+++ frontend/src/app/show/components/Fullscreen/index.tsx
@@ -2,6 +2,7 @@
 
 import { FaExpandArrowsAlt } from "react-icons/fa";
 import styles from "./fullscreen.module.css";
+import icon from "./fullscreen.png";
 
 export default function FullscreenButton(): React.JSX.Element {
 
