c031837aa48e21bf9beb9ec0a7621136c62d3221
Sven Finn <77897281+SvenFinn@users.noreply.github.com>
2025-08-15 20:31:49 +0200

    fix: Prevent text overflows

--- frontend/src/app/show/components/Show/Logo/index.tsx
+++ frontend/src/app/show/components/Show/Logo/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import styles from './logo.module.css';
 import { useHost } from '@frontend/app/hooks/useHost';
 
@@ -8,12 +8,11 @@ export default function Logo(): React.JSX.Element {
     const [cacheBlock, setCacheBlock] = useState<number>(0);
     const host = useHost();
 
-
     const logo = `${host}/api/images/icon.png?cache=${cacheBlock}`;
 
     function onImageError(event: React.SyntheticEvent<HTMLImageElement, Event>) {
         if (event.currentTarget.naturalHeight === 0 && event.currentTarget.naturalWidth === 0) {
-            setTimeout(() => setCacheBlock(Date.now()), 1000);
+            setTimeout(() => setCacheBlock(Date.now()), 5000);
         }
     }
 
