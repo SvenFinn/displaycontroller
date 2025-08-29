b7ed78cd7f09667d0fa236ea014a8aa2bfc242fe
Sven Finn <77897281+SvenFinn@users.noreply.github.com>
2025-08-29 22:36:02 +0200

    fix: Check for weird stuff

--- database/local/src/entrypoint.sh
+++ database/local/src/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Exit on error. Append "|| true" if you expect an error.
+set -euo pipefail
+
 source .env
 
 DB_HOST="$(echo "$SELF_DB_URL" | cut -d'@' -f2 | cut -d':' -f1)"
@@ -10,3 +13,16 @@ do
 done
 
 npx prisma migrate deploy
+
+echo "Checking database schema..."
+diff=$(npx prisma migrate diff --from-schema-datasource ./schema.prisma --to-schema-datamodel ./schema.prisma --script | 
+        grep -sv '^--' || true | 
+        grep -sv '^/*' || true |
+        grep -sv '^$' || true)
+
+if [ -n "$diff" ]; then
+    echo -e "\e[31mError:\e[0m" >&2
+    echo "The database schema is not up to date, even after applying migrations. Please check the following diff:" >&2
+    echo "$diff" >&2
+    exit 1
+fi
