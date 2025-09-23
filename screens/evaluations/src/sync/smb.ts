83c43e18c44a52cfbe932b1053c396c626150582
Sven Finn <77897281+SvenFinn@users.noreply.github.com>
2025-09-23 23:09:42 +0200

    feat: Evaluaitons: Check SMB Fixes #168

--- screens/evaluations/src/sync/smb.ts
+++ screens/evaluations/src/sync/smb.ts
@@ -2,6 +2,13 @@ import fs from "fs";
 import { exec } from "child_process";
 import { promisify } from "util";
 import { logger } from "dc-logger";
+import dotenv from "dotenv";
+dotenv.config({ quiet: true });
+
+if (!process.env.SM_SMB_USER || !process.env.SM_SMB_PASS) {
+    logger.error("SMB credentials are not set in environment variables.");
+    process.exit(1);
+}
 
 export async function fetchSamba(host: string, folder: string): Promise<boolean> {
     if (fs.existsSync(folder)) {
@@ -10,7 +17,7 @@ export async function fetchSamba(host: string, folder: string): Promise<boolean>
     await fs.promises.mkdir(folder, { recursive: true });
     logger.info(`Fetching HTML files from ${host} to ${folder}`);
     try {
-        const { stderr } = await (promisify(exec))(`smbclient -N \\\\\\\\${host}\\\\ergebnis -c "lcd ${folder};prompt off; recurse on; mget html*;exit"`);
+        const { stderr } = await (promisify(exec))(`smbclient --user="${process.env.SM_SMB_USER}" --password="${process.env.SM_SMB_PASS}" \\\\\\\\${host}\\\\ergebnis -c "lcd ${folder};prompt off; recurse on; mget html*;exit"`);
         logger.debug(stderr);
         return true;
     } catch (e) {
