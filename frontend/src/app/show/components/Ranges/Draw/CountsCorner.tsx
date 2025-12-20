41448aa38cd847307ad74faecdb10f21335bb500
Sven Finn <77897281+SvenFinn@users.noreply.github.com>
2025-12-20 21:26:12 +0100

    fix: Overscan counts corner

--- frontend/src/app/show/components/Ranges/Draw/CountsCorner.tsx
+++ frontend/src/app/show/components/Ranges/Draw/CountsCorner.tsx
@@ -1,7 +1,11 @@
 import styles from './rangeDraw.module.css';
 
 export default function CountsCorner({ size }: { size: [number, number] }): React.JSX.Element {
-    const points = [[size[0] / 2, -size[1] / 2], [size[0] / 2, -size[1] / 2 + size[1] / 4], [size[0] / 2 - size[0] / 4, -size[1] / 2]]
+    const points = [
+        [size[0] * 1.5, -size[1] * 1.5],
+        [size[0] * 1.5, size[1] * 0.75],
+        [-size[0] * 0.75, - size[1] * 1.5]]
+        .map(point => point.map(coord => Math.ceil(coord)));
 
     return (
         <polygon points={points.map(point => point.join(", ")).join(" ")} className={styles.countsCorner} />
